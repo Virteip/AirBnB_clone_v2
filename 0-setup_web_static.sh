04bccf531a2ac4dea4aeb95aa01e99499825c2bb
Virteip <sergiopietri@gmail.com>
2020-04-09 17:18:25 -0500

    [Update] unknown requirement checker testing my patience

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,6 +14,7 @@ sudo echo "<html>
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data
 sudo chgrp -R ubuntu /data
-sudo sed -i "42i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo sed -i "42i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
+sudo service nginx stop
+sudo service nginx start
 exit 0
