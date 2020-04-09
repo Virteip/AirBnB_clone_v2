86e94d94597caa2a4c8f1eeab5012b3764d15aa9
Virteip <sergiopietri@gmail.com>
2020-04-09 17:42:11 -0500

    [Update] unknown requirement checker testing my patience

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,13 +4,7 @@ sudo apt-get -y update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+sudo echo "Holberton" > /data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data
