3a0de85a99453a3a3b75fdf3f1b667c66272a561
Virteip <sergiopietri@gmail.com>
2020-04-09 17:10:19 -0500

    [Add]T#0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#Sets up your web servers for the deployment.
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data
+sudo chgrp -R ubuntu /data
+sudo sed -i "42i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
+sudo service nginx restart
