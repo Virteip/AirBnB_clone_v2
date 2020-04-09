08bfa0b408bcf5c2740b2de660ce158465b9255f
Virteip <sergiopietri@gmail.com>
2020-04-09 17:14:28 -0500

    [Update] 0 if success and update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #Sets up your web servers for the deployment.
+sudo apt-get -y update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
