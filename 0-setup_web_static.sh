b757ecccf68831bc8ac7cdd9d348db6c35da8732
Virteip <sergiopietri@gmail.com>
2020-04-10 18:54:12 -0500

    [Update] IDK checker won't cooperate

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,8 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo echo "Holberton" > /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
 exit 0
