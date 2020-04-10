d74fabce9d73a34ce495e2bd6aaee5850169e1ed
Virteip <sergiopietri@gmail.com>
2020-04-10 18:53:16 -0500

    [Update] IDK checker won't cooperate

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #Sets up your web servers for the deployment.
-apt-get -y update
-apt-get install -y nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "Holberton" > /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+sudo apt-get -y update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "Holberton" > /data/web_static/releases/test/index.html
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
 exit 0
