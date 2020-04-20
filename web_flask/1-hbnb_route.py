f5d8b6fdd75353f5e11099660faf1de84c039d5d
Virteip <sergiopietri@gmail.com>
2020-04-20 13:19:00 -0500

    [Fix] betty on 0 [Add] 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello():
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return 'HBNB'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
     app.url_route.strict_slashes = False
